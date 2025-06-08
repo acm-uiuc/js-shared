a24babbbd0b3697ab087a0982b648717737673cb
Dev Singh <dev@devksingh.com>
2025-06-08 16:14:01 -0400

    publish

---
+++ src/orgs.ts
@@ -0,0 +1,31 @@
+export const SIGList = [
+    "SIGPwny",
+    "SIGCHI",
+    "GameBuilders",
+    "SIGAIDA",
+    "SIGGRAPH",
+    "ICPC",
+    "SIGMobile",
+    "SIGMusic",
+    "GLUG",
+    "SIGNLL",
+    "SIGma",
+    "SIGQuantum",
+    "SIGecom",
+    "SIGPLAN",
+    "SIGPolicy",
+    "SIGARCH",
+    "SIGRobotics",
+    "SIGtricity",
+] as [string, ...string[]];
+
+export const CommitteeList = [
+    "Infrastructure Committee",
+    "Social Committee",
+    "Mentorship Committee",
+    "Academic Committee",
+    "Corporate Committee",
+    "Marketing Committee",
+] as [string, ...string[]];
+
+export const OrganizationList = ["ACM", ...SIGList, ...CommitteeList] as [string, ...string[]];
