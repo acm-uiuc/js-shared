330b5fa7b156b2d61ec4d7f60996121566c63dfe
Dev Singh <dev@devksingh.com>
2025-09-25 19:55:15 -0500

    Add membership price info

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const MembershipPriceCents = 2000
+export const MembershipPriceString = (MembershipPriceCents / 100).toLocaleString("en-US", { style: "currency", currency: "USD" });
+export const MembershipPriceIdStripe = "price_1MUGIRDiGOXU9RuSChPYK6wZ"
