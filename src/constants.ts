6aefcb06156a98893c0d0fdefeb244f75227709f
Dev Singh <dev@devksingh.com>
2026-02-10 14:35:10 -0600

    Add email override, run lint

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
-export const MembershipPriceCents = 3000
-export const MembershipPriceString = (MembershipPriceCents / 100).toLocaleString("en-US", { style: "currency", currency: "USD" });
-export const MembershipPriceIdStripe = "price_1SBNJlDiGOXU9RuSjHO7cwMX"
+export const MembershipPriceCents = 3000;
+export const MembershipPriceString = (
+  MembershipPriceCents / 100
+).toLocaleString("en-US", { style: "currency", currency: "USD" });
+export const MembershipPriceIdStripe = "price_1SBNJlDiGOXU9RuSjHO7cwMX";
--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,9 @@
-
 type ValueOf<T> = T[keyof T];
 
 export type EnsureUniqueValues<T extends Record<string, string>> = {
-    [K in keyof T]: ValueOf<T> extends T[K]
+  [K in keyof T]: ValueOf<T> extends T[K]
     ? T[K] extends ValueOf<Omit<T, K>>
-    ? never
-    : T[K]
-    : T[K]
+      ? never
+      : T[K]
+    : T[K];
 };
